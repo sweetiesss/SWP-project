9f879702321668afd8226bec21aca6f2caf20cc6
opium2345 <nhatthmse173446@fpt.edu.vn>
2023-06-18 13:56:09 +0700

    Add Create and Create page using API

--- SWP-Frontend-Admin/Controllers/HomeController.cs
+++ SWP-Frontend-Admin/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SWP_Frontend_Admin.Models;
+using System.Text;
 
 namespace SWP_Frontend_Admin.Controllers
 {
     public class HomeController : Controller
     {
+        
         public async Task<IActionResult> Index()
         {
             List<Assignment> AssignmentList = new List<Assignment>();
@@ -20,5 +22,25 @@ namespace SWP_Frontend_Admin.Controllers
             ViewData["AssignList"] = AssignmentList;
             return View(AssignmentList);
         }
+
+        public ViewResult AddAssignment() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> AddAssignment(Assignment Assignment)
+        {
+            Assignment receivedAssignment = new Assignment();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(Assignment), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("https://localhost:7219/api/assignments", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    receivedAssignment = JsonConvert.DeserializeObject<Assignment>(apiResponse);
+                }
+            }
+            return View(receivedAssignment);
+        }
+
     }
 }
